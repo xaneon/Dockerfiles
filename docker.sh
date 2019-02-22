dfc431c75f61dcca7fba15f02e4b03ea163e4edf
Bonne <bonne.habekost@gmail.com>
2019-02-22 09:58:39 +0100

    added simple dockerfile with ssh and user

--- docker.sh
+++ docker.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 docker rmi --force myalpine
 docker rmi --force alpine:3.9
-docker container prune --force
 docker image prune --force
 docker container prune --force
+docker images
