a99e8440c4b8b3f85145063459cc6f1cdc51a40c
Bonne <bonne.habekost@gmail.com>
2019-02-28 09:56:46 +0100

    added also ssh port mapping

--- start_jupyter_with_port.sh
+++ start_jupyter_with_port.sh
@@ -1,1 +1,1 @@
-docker run -p 9999:9999 -d xaneon/alpine_x11_jupyter:latest
+docker run -p 9999:9999 -p 2222:22 -d --mount src=/home/bh/Dropbox,target=/home/alpine/Dropbox,type=bind --name cont1 xaneon/alpine_x11_jupyter:latest
