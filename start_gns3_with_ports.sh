3e04f907839d8a110e3b43b721a8af781a2c3c45
Bonne <bonne.habekost@gmail.com>
2019-04-25 15:09:52 +0200

    improved

--- start_gns3_with_ports.sh
+++ start_gns3_with_ports.sh
@@ -1,1 +1,1 @@
-docker run -p 5000:5000 -p 3080:3080  -p 2224:22 -d --mount src=/home/bh/Dropbox,target=/home/alpine/Dropbox,type=bind --name cont3 xaneon/alpine_gns3
+docker run -p 5000:5000 -p 3080:3080  -p 2224:22 -d --mount src=/home/bh/Dropbox,target=/home/alpine/Dropbox,type=bind --name cont3 xaneon/alpine_gns3_server
